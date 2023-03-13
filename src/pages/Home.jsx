7cdbbdcd0b2fa25c3d7679cbb134e094f7135378
Otávio Cleyderman <otavio.faria01@fatec.sp.gov.br>
2023-03-13 19:32:43 -0300

    (13/03) Criação dos cards e inserido os dados da API de produtos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import { useRequestApi } from '../hooks/useRequestApi'
+import { AiFillHeart } from 'react-icons/ai'
 
 function home() {
   const { data: products, isRequest } = useRequestApi('https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e')
@@ -8,7 +9,37 @@ function home() {
   return (
     <>
       <Header />
-      <ul>
+      <main className='container'>
+        <span>Home</span>
+        <div className="cards">
+          {isRequest && <p> Carregando... </p> }
+          {products?.map(product => {
+            return (
+              <div className="cards__item" key={product.id}>
+                <div className="cards__fav"><AiFillHeart /></div>
+                <span className="cards__id" style={{display: "none"}}>{product.id}</span>
+                <div className="cards__image">
+                  <img src={product.image} alt="" />
+                </div>
+                <div className="cards__title">
+                  {product.title}
+                </div>
+                <div className="cards__price">
+                  R$ {product.price.toFixed(2)}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default home
+
+/* 
+
         {isRequest && <p> Carregando... </p> }
         {products?.map(product => {
           return (
@@ -18,9 +49,5 @@ function home() {
             </li>
           )
         })}
-      </ul>
-    </>
-  )
-}
 
-export default home
+*/
